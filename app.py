8d4673f0a30cfd32d3fabcf574e569b6e8682b72
JARP <105678913+anderson895@users.noreply.github.com>
2025-04-29 20:30:50 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import webbrowser
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, abort, Response
 
 app = Flask(__name__)
 
@@ -8,14 +8,45 @@ app = Flask(__name__)
 pdf_dir = os.path.join(os.path.dirname(__file__), 'static', 'pdf')
 pdf_filename = 'sequence-practice-problems.pdf'
 
-# Route to serve the PDF
+# Route to serve the PDF and dynamically set the title
 @app.route('/')
 def open_pdf():
-    return send_from_directory(pdf_dir, pdf_filename)
+    pdf_path = os.path.join(pdf_dir, pdf_filename)
+    if not os.path.exists(pdf_path):
+        # If the file doesn't exist, return a 404 error
+        abort(404)
+    
+    # HTML content with dynamic title, and full-screen PDF display
+    html_content = f'''
+    <html>
+        <head>
+            <title>{pdf_filename}</title>
+            <style>
+                html, body {{
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                    overflow: hidden;
+                }}
+                object {{
+                    width: 100%;
+                    height: 100%;
+                }}
+            </style>
+        </head>
+        <body>
+            <object data="/static/pdf/{pdf_filename}" type="application/pdf">
+                <p>Your browser does not support PDF viewing. <a href="/static/pdf/{pdf_filename}">Download the PDF</a>.</p>
+            </object>
+        </body>
+    </html>
+    '''
+    # Return the HTML response with the title and embedded PDF
+    return Response(html_content, content_type='text/html')
 
 if __name__ == '__main__':
-    # Automatically open the PDF when the app starts using relative path
-    webbrowser.open(f'file:///{os.path.join(pdf_dir, pdf_filename)}')
+    # Automatically open the PDF URL when the app starts
+    webbrowser.open('http://127.0.0.1:5000/')
     
     # Run the Flask app
     app.run(debug=True)
